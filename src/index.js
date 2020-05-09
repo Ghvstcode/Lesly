5785ab40289d759e1d16c52d03519b66c204b327
Tobax <impoissants@gmail.com>
2020-05-09 15:48:35 +0100

    Created custom types

--- src/index.js
+++ src/index.js
@@ -3,19 +3,43 @@ import {GraphQLServer} from 'graphql-yoga'
 //Type definitions
 const typeDefs = `
     type Query  {
-        hello: String!
+        me: User!
+        post: Post!
+    }
+
+    type User {
+        id: ID!
         name: String!
+        email: String!
+        age: Int
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
     }
 `
 
 //Resolvers
 const resolvers = {
     Query: {
-        hello() {
-            return 'This is my first query'
+        me() {
+            return {
+                id: 1234987,
+                name: "verse",
+                email: "verse@gmail.com",
+                age: 28
+            }
         },
-        name() {
-            return 'Oluwatobi'
+        post () {
+            return {
+                id: 1234987,
+                title: "How to Graph like a QL",
+                body: "graphing like a ql is not a easy thing however with determination you will be able to graph like a QL",
+                published: false
+            }
         }
     }
 }
