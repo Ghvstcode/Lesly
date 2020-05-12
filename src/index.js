c1e774d6817361528b896b484fec2420294ed7e2
Tobax <impoissants@gmail.com>
2020-05-12 15:33:59 +0100

    Input Type

--- src/index.js
+++ src/index.js
@@ -73,9 +73,28 @@ const typeDefs = `
     }
 
     type Mutation {
-        createUser(name: String!, email: String!, age: Int): User!
-        createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
-        createComment(text: String!, author:ID!, post:ID!): Comment!
+        createUser(data: createUserInput): User!
+        createPost(data: createPostInput): Post!
+        createComment(data: createCommentInput): Comment!
+    }
+
+    input createUserInput {
+        name: String!, 
+        email: String!, 
+        age: Int
+    }
+
+    input createPostInput {
+        title: String!, 
+        body: String!, 
+        published: Boolean!, 
+        author: ID!
+    }
+
+    input createCommentInput {
+        text: String!, 
+        author:ID!, 
+        post:ID!
     }
 
     type User {
@@ -147,23 +166,20 @@ const resolvers = {
     },
     Mutation: {
         createUser(parent, args, ctx, info) {
-            const emailTaken = users.some((user)=>user.email === args.email)
+            const emailTaken = users.some((user)=>user.email === args.data.email)
             if(emailTaken){
                 throw new Error("Email Taken")
             }
             const user = {
                 id: uuidv4(),
-                name: args.name,
-                email: args.email,
-                age: args.age
+                ...args.data
             }
-
             users.push(user)
 
             return user 
         },
         createPost(parent, args, ctx, info){
-            const userExists = users.some((user)=> user.id === args.author)
+            const userExists = users.some((user)=> user.id === args.data.author)
 
             if(!userExists) {
                 throw new Error("User not found")
@@ -171,18 +187,15 @@ const resolvers = {
 
             const post = {
                 id: uuidv4(),
-                title: args.title,
-                body: args.body,
-                published: args.published,
-                author: args.author
+                ...args.data,
             }
 
             posts.push(post)
             return post
         },
         createComment(parent, args, ctx, info) {
-            const userExists = users.some((user)=> user.id === args.author)
-            const postExists = posts.some((post)=> post.id === args.post && post.published)
+            const userExists = users.some((user)=> user.id === args.data.author)
+            const postExists = posts.some((post)=> post.id === args.data.post && post.published)
 
             if(!userExists || !postExists) {
                 throw new Error ("unable to post comment")
@@ -190,9 +203,7 @@ const resolvers = {
 
             const comment = {
                 id: uuidv4(),
-                text: args.text,
-                post: args.post,
-                author: args.author
+                ...args.data
             }
 
             comments.push(comment)
